eaf20832c27104bbfbde014abf904661d9ab5e25
arnel <arne.landwehr@innoq.com>
2020-01-24 10:33:15 +0100

    SWDBECOM-150 use heroku dyno metadata as commit info source

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/resources')));
 
 app.use('/healthcheck', require('express-healthcheck')());
-app.use('/git', require('./controllers/gitInfoController'));
+app.use('/heroku', require('./controllers/herokuController'));
 app.use('/demoshop', require('./routes/demoshopRoutes'));
 
 app.use(bodyParser.json());
