7a4346c6c52789809a92f0f24378e5d19707baa3
Felix Knopf <felix.knopf@outlook.com>
2021-03-29 15:47:04 +0200

    test

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import { herokuService } from "./services/heroku.service";
 
 // dotenv | declare before custom imports / routes
 // console.log('ENV: ' + process.env.NODE_ENV)
+console.log(path.join(__dirname, `config/${process.env.NODE_ENV}.env`))
 // dotenv.config({ path: path.join(__dirname, `config/${process.env.NODE_ENV}.env`) });
 
 // vars
